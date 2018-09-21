542a3dc4b50f12d0c99ff4a2e969c995534dd2b0
bamelegari <bamelegari@gmail.com>
2018-09-20 19:56:21 -0800

    possibly logically sound algorithm. speed testing on other machine

--- homework/HW2/build.cpp
+++ homework/HW2/build.cpp
@@ -3,9 +3,92 @@
 // See build.hpp for description.
 #include "build.hpp"
 
+// checks if two bridges cross or have a common point
+bool builder::pairIsBad(const Bridge & a, const Bridge & b)
+{
+	// check for bridges with common cities
+	if (a[0] == b[0] || a[1] == b[1])
+		return true;
+
+	// check for overlapping bridges
+	if((a[0] < b[0] && a[1] > b[1]) || (a[0] > b[0] && a[1] < b[1]))
+		return true;
+
+	return false;
+}
 
 
+// checks if given set of bridges has any pairs that cross or share a city,
+// both of which violate our constraints.
+bool builder::badBridges(vector<Bridge> bridges)
+{
+	// base case
+	if (bridges.size() == 1)
+	{
+		return false;
+	}
+
+	// Check all bridge pairs (a, b) where a = the last bridge in the vector.
+	// If those pairs cross or have any common start/end points, stop.
+	int last = bridges.size() - 1;
+	for(int i = last - 1; i >= 0; --i)
+	{
+		if(pairIsBad(bridges[last], bridges[i]))
+		{
+			return true;
+		} 
+	}
+
+	// if no pairs are bad, do same without the last bridge
+	bridges.pop_back();
+	return badBridges(bridges);
+}
+
+void builder::checkNewToll()
+{
+	// check if toll is greater than the greatest so far. If so, 
+	// make it the greatest so far.
+	int tempToll = 0;
+	for(auto i : workingSet)
+	{
+		tempToll += i[2];
+	}
+	if(tempToll > maxToll)
+		maxToll = tempToll;
+}
+
+// Recursive backtracking
+int builder::build()
+{	
+	// If we're out of places to go, base case.
+	if(workingSet.size() == bridges.size())
+	{
+		return 0;
+	}
+
+	int startPoint = bridges.size() - 1 - workingSet.size();
+	for(int i = startPoint; i >= 0; --i)
+	{
+		workingSet.push_back(bridges[i]);
+
+		if(!badBridges(workingSet))
+		{
+			checkNewToll();
+			build();
+		}
+
+		workingSet.pop_back();
+	}
+
+	return maxToll;
+	
+}
+
+// w = # of cities on West bank, e = # of cities on East bank
+// given these and a vector of bridges with tolls, and return
+// the maximum toll possible without overlapping bridges.
 int build(int w, int e, const vector<Bridge> & bridges)
 {
-	return 4;
+	builder b(w, e, bridges);
+	return b.build();
 }
--- homework/HW2/build.hpp
+++ homework/HW2/build.hpp
@@ -3,16 +3,51 @@
 // Implements an exhaustive search solution to the specifications described
 // in HW2.pdf.
 
-
-//TODO add guards
-
+#pragma once
 
 #include <vector>
 
 using std::vector;
 using Bridge = vector<int>;
 
+
+// Holds data for the recursive algorithm to work with, as well
+// as the algorithm and its helper functions
+struct builder
+{
+	builder(int w, int e, const vector<Bridge> & bridges):
+		w(w), e(e), bridges(bridges), maxToll(0) {
+
+			//start working set with first bridge
+			//workingSet.push_back(bridges[0]);
+		}
+
+	// recursive solution finder. called from client code
+	int build();
+
+	int w;
+	int e;
+	const vector<Bridge> & bridges;
+	vector<Bridge> workingSet;
+	int maxToll;
+
+private:
+	// checks if two bridges cross or have a common point
+	bool pairIsBad(const Bridge & a, const Bridge & b);
+
+	// checks if given set of bridges has any pairs that cross or share a city,
+	// both of which violate our constraints.
+	bool badBridges(vector<Bridge> bridges);
+
+	void checkNewToll();
+
+};
+
+
 // w = # of cities on West bank, e = # of cities on East bank
-// given these and a vector of bridges with tolls, and return
-// the maximum toll possible without overlapping bridges.
+// given these and a vector of bridges with tolls, return
+// the maximum toll possible within constraints.
 int build(int w, int e, const vector<Bridge> & bridges);
+
+
+
