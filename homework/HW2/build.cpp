00e2c0ebc62f160a9d644affc11e92a72a1efdfc
bamelegari <bamelegari@gmail.com>
2018-09-20 17:06:55 -0800

    add test folder, bad bridge checker working in test, now to start work in build.cpp

--- homework/HW2/build.cpp
+++ homework/HW2/build.cpp
@@ -1,3 +1,11 @@
 // build.cpp
 // Bryce Melegari, UAF CS411 Asssignment 2
 // See build.hpp for description.
+#include "build.hpp"
+
+
+
+int build(int w, int e, const vector<Bridge> & bridges)
+{
+	return 4;
+}
--- homework/HW2/build.hpp
+++ homework/HW2/build.hpp
@@ -4,4 +4,15 @@
 // in HW2.pdf.
 
 
+//TODO add guards
 
+
+#include <vector>
+
+using std::vector;
+using Bridge = vector<int>;
+
+// w = # of cities on West bank, e = # of cities on East bank
+// given these and a vector of bridges with tolls, and return
+// the maximum toll possible without overlapping bridges.
+int build(int w, int e, const vector<Bridge> & bridges);
