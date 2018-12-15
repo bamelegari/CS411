195179cec11d1a44042f465d1c3ff31b589809b4
bamelegari <bamelegari@gmail.com>
2018-12-14 22:53:34 -0900

    fixed typo in setup.py

--- homework/HW7/setup.py
+++ homework/HW7/setup.py
@@ -7,5 +7,5 @@ setup(name='maxFlow',
 		description='demonstration of Edmonds-Karp algorithm',
 		url='http://www.github.com/bamelegari/CS411',
 		author='Bryce Melegari',
-		install_requires=['networkx==2.2', 'matplotlib==2.2.3']
+		install_requires=['networkx==2.2', 'matplotlib==2.2.3'],s
 		zip_safe=False)
