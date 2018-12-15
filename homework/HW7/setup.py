d0adbfc5db26518c5fa991a97baf6f4e191af148
bamelegari <bamelegari@alaska.edu>
2018-12-14 22:47:25 -0900

    attempt at setup file to handle dependencies

---
+++ homework/HW7/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name='maxFlow',
+		version='1.0',
+		description='demonstration of Edmonds-Karp algorithm',
+		url='http://www.github.com/bamelegari/CS411',
+		author='Bryce Melegari',
+		install_requires=['networkx==2.2', 'matplotlib==2.2.3']
+		zip_safe=False)
